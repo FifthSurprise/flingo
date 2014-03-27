3e9540445ca9f4149cbce8308f8d3e4416df901a
Kevin Chang <kevin.w.chang@gmail.com>
2014-03-27 10:28:26 -0400

    Load basic shuffled wordList into board

--- js/bingo.js
+++ js/bingo.js
@@ -6,14 +6,33 @@ $(document).ready(function() {
         board[i] = new Array(5);
     }
 
-    var wordList = Array(24);
+    var wordList = shuffle(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
+        "twenty-one", "twenty-two", "twenty-three", "twenty-four"
+    ]);
 
-
-    //prototype the cells
-    var bingo_Cell = {
-        //checked (boolean)
-        //text
-    };
-
+    //Iterate through the wordlist to load the board
+    for (var row = 0; row < board.length; row++) {
+        for (var col = 0; col < board[0].length; col++) {
+            //exception for the center square
+            if (row == 3 && col == 3) {
+                board[row][col] = createCell("FREE");
+            } else {
+                board[row][col] = createCell(wordList.pop());
+            }
+        }
+    }
     //register events for listeners
 });
+
+function createCell(newtext) {
+    return {
+        text: newtext,
+        status: false
+    }
+}
+
+function shuffle(o) {
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
