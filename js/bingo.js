e9af71344bb58423d7ae85230480fb9129c4eb45
Kevin Chang <kevin.w.chang@gmail.com>
2014-03-27 10:17:24 -0400

    some basic stuff

---
+++ js/bingo.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    //create the bingo board
+    var board = new Array(5);
+    for (var i = 0; i < board.length; i++) {
+        board[i] = new Array(5);
+    }
+
+    var wordList = Array(24);
+
+
+    //prototype the cells
+    var bingo_Cell = {
+        //checked (boolean)
+        //text
+    };
+
+    //register events for listeners
+});
