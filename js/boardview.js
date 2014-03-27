00bf2a255a26cb638c7e8129196bb4c51d325ce1
Eugene Millerman <e.v.millerman@gmail.com>
2014-03-27 11:51:55 -0400

    MOAR FRONT END

--- js/boardview.js
+++ js/boardview.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   
 
   // For testing only ************************************
+  seedData();
   $("#seed_btn").on("click",function(){
     seedData();
   })
@@ -17,12 +18,23 @@ function getCell(row,col){
 
 // For testing only
 function seedData(){
+  var content = getContent();
   for(var x = 0;x<5;x++){
-      for(var y=0;y<5;y++){
-        getCell(x,y).append(makeid());
-      }
-    }  
+    for(var y=0;y<5;y++){
+      getCell(x,y).append("<span>"+content.pop()+"</span>");
+    }
+  }  
 }
 
+function getContent(){
+  var content_array = ["What?","Does that work?","That didn’t work.","Really!?",
+    "At the end of the day…","That’s ridiculous!","Ruby/Rails is awesome.",
+    "Whomp whomp.","Tada!","_____ is making my head explode","Any questions?",
+    "Stupid programmers.","Good job!","This is so cool!","Avi dismisses a question",
+    "Avi forgets to turn on join.me/record.","Avi’s hipchat window pops up",
+    "Avi Curses","Avi asks Logan a question","Does that work?","That didn’t work.",
+    "Really!?","At the end of the day…","That’s ridiculous!","What?"];
+  return content_array;
+}
 
 
